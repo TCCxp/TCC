88f21192da0c8019edc1f26a507333c7a0e6b790
elsonlj <elson.lj@gmail.com>
2014-04-13 16:50:45 -0300

    Texto pagina principal
    pequena apresentação na pagina principal

--- application/views/principal.php
+++ application/views/principal.php
@@ -1,22 +1,22 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 <html>
     <head>
         <title>TCC</title>
         <style>
-            
+
             #form_login{
-			padding-top: 15px;
-			padding-left: 15px;
-			border-left: 20%;
-			margin-top: 10px;
-			display: block;
-			width: 700px;
-			background-color: #FFF;
-			height: auto;
-			border:double;
-			border-radius: 10px;
-			font:"Arial Black", Gadget, sans-serif;
-			font-size:16px;
+                padding-top: 15px;
+                padding-left: 15px;
+                border-left: 20%;
+                margin-top: 10px;
+                display: block;
+                width: 700px;
+                background-color: #FFF;
+                height: auto;
+                border:double;
+                border-radius: 10px;
+                font:"Arial Black", Gadget, sans-serif;
+                font-size:16px;
             }
 
         </style>
@@ -25,16 +25,27 @@
         <div id="cabecalho">
             <div id="login">
                 <?php echo validation_errors(); ?>
-                <?php echo form_open('autentica');?>
+                <?php echo form_open('autentica'); ?>
                 <label for="name">Login:</label>
                 <input type="text" name="login" id="login" value="" /><br><br>
                 <label for="name">Senha:</label>
                 <input type="password" name="senha" id="senha" value="" /><br><br>
                 <input type="submit" value="Login" name="Btn_Login"/>
-                <?php echo form_close();?>
+                <?php echo form_close(); ?>
                 <a href="principal/novo_usuario"> Cadastre - se aqui!</a>
             </div>
         </div>
-        
+        <div id="conteudo">
+            <p>
+                TCC<br>
+                Aplicação TCC
+                Aplicação desenvolvida utilizando a metodologia eXtreme Programming, onde foram aplicadas as técnicas de:<br>
+                Elaboração de histórias de usuário Test-first Desenvolvimento Incremental Programação em pares<br>
+                Alunos:<br>
+                Diego Henrique Presner<br>
+                Elson Luiz dos Santos Junior<br>
+            <p>
+        </div>
+
     </body>
 </html>
